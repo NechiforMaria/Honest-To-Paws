3e6e411d4f8c2598e1d7e7f40a605a2b3ddc5f09
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-10-05 13:16:23 +0300

    Click on all articles

--- tests/Articles.js
+++ tests/Articles.js
@@ -1,112 +1,29 @@
 import Menu from "../page-objects/components/Menu";
-import AllArticles from "../page-objects/pages/AllArticles";
+import Homepage from "../page-objects/pages/Homepage";
 import HomePage from "../page-objects/pages/Homepage";
 
 describe("All the articles", () => {
   beforeEach(async () => {
-    HomePage.openHomePage();
+    browser.url("https://astage.honesttopaws.com/category/cute");
     Menu.waitForExistHeader();
     Menu.clickOnApproveButton();
     Menu.clickOnApproveButtonFirefox();
   });
 
-  it("Click on the all articles", async () => {
-    await (await Menu.cuteButton).waitForDisplayed();
-    await (await Menu.cuteButton).click();
-    const titleArticles = await $$(".c-link__cover");
-
-    titleArticles.forEach(async (titleArticles) => {
-      {
-        await titleArticles.click();
-        console.log(await titleArticles.getText());
-        await Homepage.backPage();
-      }
-    });
-    //AllArticles.allArticlesLinks();
-    //   await (await AllArticles.firstArticle).waitForExist();
-    //   await (await AllArticles.firstArticle).click();
-    //   await await expect(browser).toHaveUrlContaining("people-looked-past-pff");
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.secondArticle).waitForExist();
-    //   await (await AllArticles.secondArticle).click();
-    //   await await expect(browser).toHaveUrlContaining("get-jinxed");
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.thirdArticle).waitForExist();
-    //   await (await AllArticles.thirdArticle).click();
-    //   await await expect(browser).toHaveUrlContaining(
-    //     "iconic-films-backstage-photos-copy-copy-copy-copy"
-    //   );
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.fourthArticle).waitForExist();
-    //   await (await AllArticles.fourthArticle).click();
-    //   await await expect(browser).toHaveUrlContaining(
-    //     "iconic-films-backstage-photos-copy-copy-copy"
-    //   );
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.fifthArticle).waitForExist();
-    //   await (await AllArticles.fifthArticle).click();
-    //   await await expect(browser).toHaveUrlContaining(
-    //     "iconic-films-backstage-photos-copy-copy"
-    //   );
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.sixthArticle).waitForExist();
-    //   await (await AllArticles.sixthArticle).click();
-    //   await await expect(browser).toHaveUrlContaining(
-    //     "iconic-films-backstage-photos-copy"
-    //   );
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.seventhArticle).waitForExist();
-    //   await (await AllArticles.seventhArticle).click();
-    //   await await expect(browser).toHaveUrlContaining("ecommerce");
-    //   HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.eighthArticle).waitForExist();
-    //   await (await AllArticles.eighthArticle).click();
-    //   await await expect(browser).toHaveUrlContaining(
-    //     "how-to-keep-your-health-insurance-when-furloughed-or-laid-off"
-    //   );
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.ninthArticle).waitForExist();
-    //   await (await AllArticles.ninthArticle).click();
-    //   await await expect(browser).toHaveUrlContaining("beatles-lyrics");
-    //   await HomePage.backPage();
-    //   await await expect(browser).toHaveUrl(
-    //     "https://astage.honesttopaws.com/category/cute"
-    //   );
-
-    //   await (await AllArticles.tenthArticle).waitForExist();
-    //   await (await AllArticles.tenthArticle).click();
-    //   await (await expect(browser)).toHaveUrlContaining("delusional-house-cats");
+  it("Click on all articles", async () => {
+    const articles = await $$(".c-post__content .c-link__cover");
+    console.log("articles ", await articles.length);
+    let article;
+    let articleHref;
+    for await (article of articles) {
+      articleHref = await article.getAttribute("href");
+      await Homepage.scrollDown();
+      await article.scrollIntoView({ block: "center" });
+      await article.click();
+      await browser.pause(1000);
+      await expect(browser).toHaveUrlContaining(articleHref.slice(0, -6));
+      await HomePage.backPage();
+      await browser.pause(1000);
+    }
   });
 });
