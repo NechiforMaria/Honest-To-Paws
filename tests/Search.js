5c1fbed287e1f55fcc0ca40c0337d754199bd3a7
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-10-11 13:00:02 +0300

    Run tests on multiple devices

--- tests/Search.js
+++ tests/Search.js
@@ -1,34 +1,45 @@
 const { default: Menu } = require("../page-objects/components/Menu");
-const { default: Homepage } = require("../page-objects/pages/Homepage");
+const { default: HomePage } = require("../page-objects/pages/Homepage");
 
-describe("Check the search functionality", async () => {
-  beforeEach(async () => {
-    Homepage.openHomePage();
-    Menu.waitForExistHeader();
-    Menu.clickOnApproveButton();
-    Menu.clickOnApproveButtonFirefox();
-  });
-  it("Search with valid word", async () => {
-    await (await Menu.searchButton).waitForDisplayed();
-    await (await Menu.searchButton).click();
-    const validTextToSearch = "Finish The Most Iconic Book Titles In History";
-    await (await Menu.searchButton).setValue(validTextToSearch);
-    await (await Menu.searchIcon).waitForDisplayed();
-    await (await Menu.searchIcon).click();
-    await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
-      validTextToSearch
-    );
-  });
+DEVICE == "mobile"
+  ? describe.skip
+  : describe("Check the search functionality", async () => {
+      beforeEach(async () => {
+        HomePage.openDevice("https://astage.honesttopaws.com");
+        //Homepage.openHomePage();
+        //Menu.waitForExistHeader();
+        Menu.clickOnApproveButton();
+        Menu.clickOnApproveButtonFirefox();
+      });
+      it("Search with valid word", async () => {
+        await HomePage.scrollDownToPixel100();
+        await (await Menu.searchIcon).waitForDisplayed();
+        await (await Menu.searchIcon).click();
+        browser.pause(1000);
+        await (await Menu.searchInput).waitForDisplayed();
+        await (await Menu.searchInput).click();
+        const validTextToSearch =
+          "Finish The Most Iconic Book Titles In History";
+        await (await Menu.searchInput).setValue(validTextToSearch);
+        await (await Menu.searchIcon).waitForDisplayed();
+        await (await Menu.searchIcon).click();
+        await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
+          validTextToSearch
+        );
+      });
 
-  it("Search with invalid word", async () => {
-    await (await Menu.searchButton).waitForDisplayed();
-    await (await Menu.searchButton).click();
-    const invalidTextToSearch = "qwq";
-    await (await Menu.searchButton).setValue(invalidTextToSearch);
-    await (await Menu.searchIcon).waitForDisplayed();
-    await (await Menu.searchIcon).click();
-    await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
-      `Sorry, we couldn't find any results to match that search.`
-    );
-  });
-});
+      it("Search with invalid word", async () => {
+        await (await Menu.searchIcon).waitForDisplayed();
+        await (await Menu.searchIcon).click();
+        browser.pause(1000);
+        await (await Menu.searchInput).waitForDisplayed();
+        await (await Menu.searchInput).click();
+        const invalidTextToSearch = "qwq";
+        await (await Menu.searchInput).setValue(invalidTextToSearch);
+        await (await Menu.searchIcon).waitForDisplayed();
+        await (await Menu.searchIcon).click();
+        await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
+          `Sorry, we couldn't find any results to match that search.`
+        );
+      });
+    });
