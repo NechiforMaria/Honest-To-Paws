295625224e72e926b315a48bd43ee9f0cfcbc71c
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-10-15 11:43:01 +0300

    Revised test

--- tests/Search.js
+++ tests/Search.js
@@ -1,34 +1,137 @@
-const { default: Menu } = require("../page-objects/components/Menu");
-const { default: Homepage } = require("../page-objects/pages/Homepage");
+import Menu from "../page-objects/components/Menu";
+import HomePage from "../page-objects/pages/Homepage";
 
-describe("Check the search functionality", async () => {
+describe("Checking the appearance of buttons", () => {
   beforeEach(async () => {
-    Homepage.openHomePage();
+    HomePage.openHomePage();
     Menu.waitForExistHeader();
     Menu.clickOnApproveButton();
     Menu.clickOnApproveButtonFirefox();
-  });
-  it("Search with valid word", async () => {
-    await (await Menu.searchButton).waitForDisplayed();
-    await (await Menu.searchButton).click();
-    const validTextToSearch = "Finish The Most Iconic Book Titles In History";
-    await (await Menu.searchButton).setValue(validTextToSearch);
-    await (await Menu.searchIcon).waitForDisplayed();
-    await (await Menu.searchIcon).click();
-    await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
-      validTextToSearch
-    );
+    await browser.pause(1000);
   });
 
-  it("Search with invalid word", async () => {
-    await (await Menu.searchButton).waitForDisplayed();
-    await (await Menu.searchButton).click();
-    const invalidTextToSearch = "qwq";
-    await (await Menu.searchButton).setValue(invalidTextToSearch);
-    await (await Menu.searchIcon).waitForDisplayed();
-    await (await Menu.searchIcon).click();
-    await expect(await Menu.searchResultBoxMessage).toHaveTextContaining(
-      `Sorry, we couldn't find any results to match that search.`
-    );
+  it("Checking the appearance of the menu buttons from the header", async () => {
+    const buttons = [
+      Menu.cuteButton,
+      Menu.funnyButton,
+      Menu.goodButton,
+      Menu.omgButton,
+      Menu.moreButton,
+    ];
+
+    for (let i = 0; i < buttons.length; i++) {
+      const elem = buttons[i];
+
+      const fontSize = await elem.getCSSProperty("font-size").value;
+      await expect(fontSize).toEqual("16px");
+
+      const minWidth = await elem.getCSSProperty("min-width").value;
+      await expect(minWidth).toEqual("120px");
+
+      const borderLeft = await elem.getCSSProperty("border-left-width");
+      const valueBorderLeft = await borderLeft.value.slice(0, -2);
+      await expect(Math.round(valueBorderLeft)).toEqual(2);
+
+      const borderTop = await elem.getCSSProperty("border-top-width");
+      const valueBorderTop = await borderTop.value.slice(0, -2);
+      await expect(Math.round(valueBorderTop)).toEqual(5);
+
+      const color = await elem.getCSSProperty("color");
+      const valueColor = color.parsed.hex;
+      await expect(valueColor).toEqual("#898989");
+
+      const alignItems = await elem.getCSSProperty("align-items").value;
+      await expect(alignItems).toEqual("center");
+    }
+  });
+
+  it("Checking the appearance of the submenu buttons", async () => {
+    const submenuButtons = [
+      await Menu.aboutButton,
+      Menu.privacyPolicyButton,
+      Menu.dmcaButton,
+      Menu.advertisingButton,
+    ];
+
+    await (await Menu.moreButton).waitForDisplayed();
+    await (await Menu.moreButton).click();
+    await browser.pause(1000);
+
+    for (let i = 0; i < submenuButtons.length; i++) {
+      const object = await submenuButtons[i];
+
+      let fontSize = await object.getCSSProperty("font-size").value;
+      await expect(fontSize).toEqual("16px");
+
+      const color = await object.getCSSProperty("color");
+      const valueColor = await color.parsed.hex;
+      await expect(valueColor).toEqual("#898989");
+
+      const pandding = await object.getCSSProperty("padding-right").value;
+      await expect(pandding).toEqual("10px");
+
+      const width = await object.getCSSProperty("width").value;
+      await expect(width).toEqual("150px");
+    }
+  });
+
+  it("Checking the appearance of the placeholder for the search input", async () => {
+    const search = await Menu.searchButton;
+
+    const fontSize = await search.getCSSProperty("font-size").value;
+    await expect(fontSize).toEqual("16px");
+
+    const paddingLeft = await search.getCSSProperty("padding-left").value;
+    await expect(paddingLeft).toEqual("15px");
+  });
+
+  it("Checking the appearance of the search icon", async () => {
+    const searchIcon = await Menu.searchIcon;
+
+    const backgroundPosition = await searchIcon.getCSSProperty(
+      "background-position"
+    ).value;
+    await expect(backgroundPosition).toEqual("-290px -129px");
+
+    const width = await searchIcon.getCSSProperty("width").value;
+    await expect(width).toEqual("20px");
+
+    const height = await searchIcon.getCSSProperty("height").value;
+    await expect(height).toEqual("20px");
+  });
+
+  it("Checking the appearance of the see more buttons", async () => {
+    const seeMoreButtons = [
+      HomePage.seeMoreCuteButton,
+      HomePage.seeMoreFuunyButton,
+      HomePage.seeMoreGoodeButton,
+      HomePage.seeMoreOmgButton,
+    ];
+
+    for (let i = 0; i < seeMoreButtons.length; i++) {
+      const object = await seeMoreButtons[i];
+
+      browser.pause(1000);
+      const fontSize = await object.getCSSProperty("font-size").value;
+      await expect(fontSie).toEqual("18px");
+
+      const color = await object.getCSSProperty("color");
+      const valueColor = await color.parsed.hex;
+      await expect(valueColor).toEqual("#000000");
+    }
+  });
+
+  it("Checking the appearance of the logo", async () => {
+    const logo = await Menu.logo;
+
+    const backgroundPosition = await logo.getCSSProperty("background-position")
+      .value;
+    await expect(backgroundPosition).toEqual("0px");
+
+    const width = await logo.getCSSProperty("width").value;
+    await expect(width).toEqual("335px");
+
+    const height = await logo.getCSSProperty("height").value;
+    await expect(height).toEqual("33px");
   });
 });
--- tests/UIButtons.js
+++ tests/UIButtons.js
@@ -10,7 +10,7 @@ describe("Checking the appearance of buttons", () => {
     await browser.pause(5000);
   });
 
-  it("Checking the appearance of the menu buttons", async () => {
+  it("Checking the appearance of the menu buttons from the header", async () => {
     const buttons = [
       Menu.cuteButton,
       Menu.funnyButton,
@@ -23,38 +23,28 @@ describe("Checking the appearance of buttons", () => {
       const elem = buttons[i];
 
       const fontSize = await elem.getCSSProperty("font-size");
-      console.log(fontSize);
-      const valueFontSize = fontSize.value;
-      await expect(valueFontSize).toEqual("16px");
+      await expect(fontSize.value).toEqual("16px");
 
       const minWidth = await elem.getCSSProperty("min-width");
-      console.log(minWidth);
-      const valueMinWidth = minWidth.value;
-      await expect(valueMinWidth).toEqual("120px");
+      await expect(minWidth.value).toEqual("120px");
 
       const borderLeft = await elem.getCSSProperty("border-left-width");
-      console.log(borderLeft);
       const valueBorderLeft = await borderLeft.value.slice(0, -2);
       await expect(Math.round(valueBorderLeft)).toEqual(2);
 
       const borderTop = await elem.getCSSProperty("border-top-width");
-      console.log(borderTop);
       const valueBorderTop = await borderTop.value.slice(0, -2);
       await expect(Math.round(valueBorderTop)).toEqual(5);
 
       const color = await elem.getCSSProperty("color");
-      console.log(color);
-      const valueColor = color.parsed.hex;
-      await expect(valueColor).toEqual("#898989");
+      await expect(color.parsed.hex).toEqual("#898989");
 
       const alignItems = await elem.getCSSProperty("align-items");
-      console.log(alignItems);
-      const valueAlignItems = alignItems.value;
-      await expect(valueAlignItems).toEqual("center");
+      await expect(alignItems.value).toEqual("center");
     }
   });
 
-  it("Checking the appearance of the submenu buttons", async () => {
+  it("Checking the appearance of the submenu buttons from the header", async () => {
     const submenuButtons = [
       await Menu.aboutButton,
       Menu.privacyPolicyButton,
@@ -64,71 +54,49 @@ describe("Checking the appearance of buttons", () => {
 
     await (await Menu.moreButton).waitForDisplayed();
     await (await Menu.moreButton).click();
-    await browser.pause(5000);
+    await browser.pause(1000);
 
     for (let i = 0; i < submenuButtons.length; i++) {
       const object = await submenuButtons[i];
 
-      await browser.pause(5000);
-
       let fontSize = await object.getCSSProperty("font-size");
-      console.log(fontSize);
-      const valueFontSize = await fontSize.value;
-      await expect(valueFontSize).toEqual("16px");
+      await expect(fontSize.value).toEqual("16px");
 
       const color = await object.getCSSProperty("color");
-      console.log(color);
-      const valueColor = await color.parsed.hex;
-      await expect(valueColor).toEqual("#898989");
+      await expect(color.parsed.hex).toEqual("#898989");
 
       const pandding = await object.getCSSProperty("padding-right");
-      console.log(pandding);
-      const valuePandding = await pandding.value;
-      await expect(valuePandding).toEqual("10px");
+      await expect(pandding.value).toEqual("10px");
 
       const width = await object.getCSSProperty("width");
-      console.log(width);
-      const valueWidth = await width.value;
-      await expect(valueWidth).toEqual("150px");
+      await expect(width.value).toEqual("150px");
     }
   });
 
-  it("Checking the appearance of the search button", async () => {
+  it("Checking the appearance of the placeholder for the search input", async () => {
     const search = await Menu.searchButton;
 
     const fontSize = await search.getCSSProperty("font-size");
-    console.log(fontSize);
-    const valueFontSize = await fontSize.value;
-    await expect(valueFontSize).toEqual("16px");
+    await expect(fontSize.value).toEqual("16px");
 
     const paddingLeft = await search.getCSSProperty("padding-left");
-    console.log(paddingLeft);
-    const valuePaddingLeft = await paddingLeft.value;
-    await expect(valuePaddingLeft).toEqual("15px");
+    await expect(paddingLeft.value).toEqual("15px");
   });
 
   it("Checking the appearance of the search icon", async () => {
     const searchIcon = await Menu.searchIcon;
 
-    const backgroundPosition = await searchIcon.getCSSProperty(
-      "background-position"
-    );
-    console.log(backgroundPosition);
-    const valueBackgroundPosition = await backgroundPosition.value;
-    await expect(valueBackgroundPosition).toEqual("-290px -129px");
+    const backgroundImage = await searchIcon.getCSSProperty("background-image");
+    await expect(backgroundImage).isVisible;
 
     const width = await searchIcon.getCSSProperty("width");
-    console.log(width);
-    const valueWidth = await width.value;
-    await expect(valueWidth).toEqual("20px");
+    await expect(width.value).toEqual("20px");
 
     const height = await searchIcon.getCSSProperty("height");
-    console.log(height);
-    const valueHeight = await height.value;
-    await expect(valueHeight).toEqual("20px");
+    await expect(height.value).toEqual("20px");
   });
 
-  it("Checking the appearance of the see more buttons", async () => {
+  it("Checking the appearance of the see more buttons in the middle of the page", async () => {
     const seeMoreButtons = [
       HomePage.seeMoreCuteButton,
       HomePage.seeMoreFuunyButton,
@@ -139,35 +107,24 @@ describe("Checking the appearance of buttons", () => {
     for (let i = 0; i < seeMoreButtons.length; i++) {
       const object = await seeMoreButtons[i];
 
-      browser.pause(1000);
       const fontSize = await object.getCSSProperty("font-size");
-      console.log(fontSize);
-      const valueFontSie = await fontSize.value;
-      await expect(valueFontSie).toEqual("18px");
+      await expect(fontSize.value).toEqual("18px");
 
       const color = await object.getCSSProperty("color");
-      console.log(color);
-      const valueColor = await color.parsed.hex;
-      await expect(valueColor).toEqual("#000000");
+      await expect(color.parsed.hex).toEqual("#000000");
     }
   });
 
   it("Checking the appearance of the logo", async () => {
     const logo = await Menu.logo;
 
-    const backgroundPosition = await logo.getCSSProperty("background-position");
-    console.log(backgroundPosition);
-    const valueBackgroundPosition = await backgroundPosition.value;
-    await expect(valueBackgroundPosition).toEqual("0px");
+    const backgroundImage = await logo.getCSSProperty("background-image");
+    await expect(backgroundImage).isVisible;
 
     const width = await logo.getCSSProperty("width");
-    console.log(width);
-    const valueWidth = await width.value;
-    await expect(valueWidth).toEqual("335px");
+    await expect(width.value).toEqual("335px");
 
     const height = await logo.getCSSProperty("height");
-    console.log(height);
-    const valueHeight = await height.value;
-    await expect(valueHeight).toEqual("33px");
+    await expect(height.value).toEqual("33px");
   });
 });
